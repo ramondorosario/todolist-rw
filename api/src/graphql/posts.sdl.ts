fd01a141fee2afeb0da3337114465671faa05ad3
Ramon do Rosário <ramon.rosario@cubos.io>
2024-07-28 19:51:14 -0300

    fix: type

--- api/src/graphql/posts.sdl.ts
+++ api/src/graphql/posts.sdl.ts
@@ -1,40 +1,40 @@
 export const schema = gql`
-	enum Priority {
-		low
-		medium
-		high
-	}
+  enum Priority {
+    low
+    medium
+    high
+  }
 
-	type Task {
-		id: String!
-		title: String!
-		description: String
-		dueDate: Date!
-		priority: Priority!
-	}
+  type Task {
+    id: String!
+    title: String!
+    description: String
+    dueDate: Date!
+    priority: Priority!
+  }
 
-	type Query {
-		tasks: [Task!]! @skipAuth
-		task(id: String!): Task! @skipAuth
-	}
+  type Query {
+    tasks: [Task!]! @skipAuth
+    task(id: String!): Task! @skipAuth
+  }
 
-	input CreateTaskInput {
-		title: String!
-		description: String
-		dueDate: Date!
-		priority: Priority!
-	}
+  input CreateTaskInput {
+    title: String!
+    description: String
+    dueDate: DateTime!
+    priority: Priority!
+  }
 
-	input UpdateTaskInput {
-		title: String!
-		description: String
-		dueDate: Date!
-		priority: Priority!
-	}
+  input UpdateTaskInput {
+    title: String!
+    description: String
+    dueDate: DateTime!
+    priority: Priority!
+  }
 
-	type Mutation {
-		createTask(input: CreateTaskInput!): Task! @skipAuth
-		updateTask(id: String!, input: UpdateTaskInput!): Task! @skipAuth
-		deleteTask(id: String!): Task! @skipAuth
-	}
+  type Mutation {
+    createTask(input: CreateTaskInput!): Task! @skipAuth
+    updateTask(id: String!, input: UpdateTaskInput!): Task! @skipAuth
+    deleteTask(id: String!): Task! @skipAuth
+  }
 `
